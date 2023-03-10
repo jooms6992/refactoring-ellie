71354a60039b6438d0b3dfb7b8e09f3f3dcdb130
Jooms <jooms6992@gmail.com>
2023-03-10 12:01:37 +0900

    7-1 refactoring

--- ch7/7-1.js
+++ ch7/7-1.js
@@ -1,4 +1,36 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+class Organization {
+  #name;
+  #country;
+  //   constructor(name, country) {
+  constructor(data) {
+    this.#data = data;
+    this.#name = data.name;
+    this.#country = data.country;
+  }
+  get name() {
+    return this.#name;
+  }
+  set name(value) {
+    this.#name = value;
+  }
+  get country() {
+    return this.#country;
+  }
+  set country(value) {
+    this.#country = value;
+  }
+
+  get rawData() {
+    // return {...this.#data} // 얕은 복사, cloneDeep
+    return { name: this.name, country: this.country };
+  }
+}
+
+// const organization1 = new Organization('Acme Gooseberries', 'GB');
+const organization = new Organization({
+  name: 'Acme Gooseberries',
+  country: 'GB',
+});
 
 organization.name = 'Dream Coding';
 console.log(organization.name);
