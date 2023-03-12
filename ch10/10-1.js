d391c3d44fd44eb8f8f2629c66a0210b9c2b3f65
Jooms <jooms6992@gmail.com>
2023-03-12 10:52:15 +0900

    10-1 refactoring

--- ch10/10-1.js
+++ ch10/10-1.js
@@ -1,7 +1,47 @@
 function calculateCharge(date, quantity, plan) {
-  let charge = 0;
-  if (!date.isBefore(plan.summerStart) && !date.isAfter(plan.summerEnd))
-    charge = quantity * plan.summerRate;
-  else charge = quantity * plan.regularRate + plan.regularServiceCharge;
-  return charge;
+  return isSummer() ? summerCharge() : regularCharge();
+
+  function isSummer() {
+    return !date.isBefore(plan.summerStart) && !date.isAfter(plan.summerEnd);
+  }
+
+  function summerCharge() {
+    return quantity * plan.summerRate;
+  }
+
+  function regularCharge() {
+    return quantity * plan.regularRate + plan.regularServiceCharge;
+  }
+}
+
+class Charge {
+  #date;
+  #quantity;
+  #plan;
+  constructor(date, quantity, plan) {
+    this.#date = date;
+    this.#quantity = quantity;
+    this.#plan = plan;
+  }
+
+  get isSummer() {
+    return (
+      !this.#date.isBefore(this.#plan.summerStart) &&
+      !this.#date.isAfter(this.#plan.summerEnd)
+    );
+  }
+
+  get base() {
+    return this.isSummer() ? this.summer() : this.reguler();
+  }
+
+  get summer() {
+    return this.#quantity * this.#plan.summerRate;
+  }
+
+  get regular() {
+    return (
+      this.#quantity * this.#plan.regularRate + this.#plan.regularServiceCharge
+    );
+  }
 }
